8295468a0a15bc0105bc3c6d9d3a466621c2c61f
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2023-02-03 12:37:19 +0000

    Basic player movement using C++

--- Source/AdvancedPrototype/TP_ThirdPerson/TP_ThirdPersonCharacter.cpp
+++ Source/AdvancedPrototype/TP_ThirdPerson/TP_ThirdPersonCharacter.cpp
@@ -13,46 +13,46 @@
 
 ATP_ThirdPersonCharacter::ATP_ThirdPersonCharacter()
 {
-	// Set size for collision capsule
-	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+	PrimaryActorTick.bCanEverTick = true;
 
-	// set our turn rate for input
-	TurnRateGamepad = 50.f;
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 
-	// Don't rotate when the controller rotates. Let that just affect the camera.
-	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
-	bUseControllerRotationRoll = false;
 
-	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+	camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	//camera->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
 
-	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
-	// instead of recompiling to adjust them
-	GetCharacterMovement()->JumpZVelocity = 700.f;
-	GetCharacterMovement()->AirControl = 0.35f;
-	GetCharacterMovement()->MaxWalkSpeed = 500.f;
-	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
-	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+	arm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 
-	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+	arm->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	arm->TargetArmLength = 300.f;
+	arm->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
 
-	// Create a follow camera
-	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+	arm->bEnableCameraLag = true;
+	arm->CameraLagSpeed = 2;
+	arm->CameraLagMaxDistance = 1.5f;
+
+	arm->bEnableCameraRotationLag = true;
+	arm->CameraRotationLagSpeed = 4;
+	arm->CameraLagMaxTimeStep = 1;
+
+	camera->AttachToComponent(arm, FAttachmentTransformRules::KeepRelativeTransform);
+
+	jumping = false;
 
 	levels.Add("ThirdPersonMap");
-	levels.Add("ThirdPersonMap2");
+	levels.Add("StarterMap");
 	levels.Add("Minimal_Default");
+}
 
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+void ATP_ThirdPersonCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (jumping)
+	{
+		Jump();
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -60,27 +60,65 @@ ATP_ThirdPersonCharacter::ATP_ThirdPersonCharacter()
 
 void ATP_ThirdPersonCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
 {
-	// Set up gameplay key bindings
-	check(PlayerInputComponent);
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
-	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis("Move Forward / Backward", this, &ATP_ThirdPersonCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("Move Right / Left", this, &ATP_ThirdPersonCharacter::MoveRight);
+	InputComponent->BindAxis("Horizontal", this, &ATP_ThirdPersonCharacter::HorizontalMove);
+	InputComponent->BindAxis("Vertical", this, &ATP_ThirdPersonCharacter::VerticalMove);
+	InputComponent->BindAxis("HorizontalRotation", this, &ATP_ThirdPersonCharacter::HorizontalRotation);
+	InputComponent->BindAxis("VerticalRotation", this, &ATP_ThirdPersonCharacter::VerticalRotation);
 
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "turn" handles devices that provide an absolute delta, such as a mouse.
-	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	PlayerInputComponent->BindAxis("Turn Right / Left Mouse", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("Turn Right / Left Gamepad", this, &ATP_ThirdPersonCharacter::TurnAtRate);
-	PlayerInputComponent->BindAxis("Look Up / Down Mouse", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("Look Up / Down Gamepad", this, &ATP_ThirdPersonCharacter::LookUpAtRate);
-
-	// handle touch devices
-	PlayerInputComponent->BindTouch(IE_Pressed, this, &ATP_ThirdPersonCharacter::TouchStarted);
-	PlayerInputComponent->BindTouch(IE_Released, this, &ATP_ThirdPersonCharacter::TouchStopped);
+	InputComponent->BindAction("Jump", IE_Pressed, this, &ATP_ThirdPersonCharacter::CheckJump);
+	InputComponent->BindAction("Jump", IE_Released, this, &ATP_ThirdPersonCharacter::CheckJump);
 
 	PlayerInputComponent->BindAxis("ChangeLevel", this, &ATP_ThirdPersonCharacter::SwitchLevel);
+}
+
+void ATP_ThirdPersonCharacter::HorizontalMove(float value)
+{
+	if (value)
+	{
+		AddMovementInput(GetActorRightVector(), value);
+	}
+}
+
+void ATP_ThirdPersonCharacter::VerticalMove(float value)
+{
+	if (value)
+	{
+		AddMovementInput(GetActorForwardVector(), value);
+	}
+}
+
+void ATP_ThirdPersonCharacter::HorizontalRotation(float value)
+{
+	if (value)
+	{
+		AddActorLocalRotation(FRotator(0, value, 0));
+	}
+}
+
+void ATP_ThirdPersonCharacter::CheckJump()
+{
+	if (jumping)
+	{
+		jumping = false;
+	}
+	else
+	{
+		jumping = true;
+	}
+}
+
+void ATP_ThirdPersonCharacter::VerticalRotation(float value)
+{
+	if (value)
+	{
+		float temp = arm->GetRelativeRotation().Pitch + value;
+		if (temp < 25 && temp > -65)
+		{
+			arm->AddLocalRotation(FRotator(value, 0, 0));
+		}
+	}
 }
 
 void ATP_ThirdPersonCharacter::SwitchLevel(float direction)
@@ -106,56 +144,5 @@ void ATP_ThirdPersonCharacter::SwitchLevel(float direction)
 				break;
 			}
 		}
-	}
-}
-
-void ATP_ThirdPersonCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
-{
-	Jump();
-}
-
-void ATP_ThirdPersonCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
-{
-	StopJumping();
-}
-
-void ATP_ThirdPersonCharacter::TurnAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
-}
-
-void ATP_ThirdPersonCharacter::LookUpAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
-}
-
-void ATP_ThirdPersonCharacter::MoveForward(float Value)
-{
-	if ((Controller != nullptr) && (Value != 0.0f))
-	{
-		// find out which way is forward
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-		// get forward vector
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-		AddMovementInput(Direction, Value);
-	}
-}
-
-void ATP_ThirdPersonCharacter::MoveRight(float Value)
-{
-	if ( (Controller != nullptr) && (Value != 0.0f) )
-	{
-		// find out which way is right
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-	
-		// get right vector 
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-		// add movement in that direction
-		AddMovementInput(Direction, Value);
 	}
 }
